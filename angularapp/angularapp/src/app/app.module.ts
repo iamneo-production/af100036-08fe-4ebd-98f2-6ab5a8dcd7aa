dad12a4cd594d149f9579c8e6c781a94d3317068
dhivyas24 <dhivyas2424@gmail.com>
2023-06-19 11:13:41 +0000

    commit

--- angularapp/angularapp/src/app/app.module.ts
+++ angularapp/angularapp/src/app/app.module.ts
@@ -4,14 +4,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AddemployerformComponentComponent } from './admin-dashboard/addemployerform-component/addemployerform-component.component';
+
+import { AddjobformComponentComponent } from './admin-dashboard/addjobform-component/addjobform-component.component';
+import { AddjobseekerformComponentComponent } from './admin-dashboard/addjobseekerform-component/addjobseekerform-component.component';
+import { CmsComponentComponent } from './admin-dashboard/cms-component/cms-component.component';
+import { EmployeranalyticsComponentComponent } from './admin-dashboard/employeranalytics-component/employeranalytics-component.component';
+
+import { JobcompoentComponentComponent } from './admin-dashboard/jobcompoent-component/jobcompoent-component.component';
+import { JobseekercompoentComponentComponent } from './admin-dashboard/jobseekercompoent-component/jobseekercompoent-component.component';
+import { EmployercomponentComponent } from './admin-dashboard/employercomponent/employercomponent.component';
+import { FaqformComponentComponent } from './admin-dashboard/faqform-component/faqform-component.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminDashboardComponent
+    AddjobformComponentComponent,
+    CmsComponentComponent,
+    EmployeranalyticsComponentComponent,
+    JobcompoentComponentComponent,
+    JobseekercompoentComponentComponent,
+    AddjobseekerformComponentComponent,
+    AddemployerformComponentComponent,
+    AdminDashboardComponent,
+    EmployercomponentComponent,
+    FaqformComponentComponent
+  
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
