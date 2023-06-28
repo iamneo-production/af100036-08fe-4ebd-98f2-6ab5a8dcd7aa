8885a4eeb0a4f646e7e9100427e39d1f887a41fe
AbhishekJha45 <abhishekjha4123@gmail.com>
2023-06-28 19:00:31 +0000

    job search page

--- job-portal-frontend/src/app/app-routing.module.ts
+++ job-portal-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { JobSearchComponent } from './job-search/job-search.component';
+const routes: Routes = [
+  {path: '', component: JobSearchComponent, title:'Job Search Page'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const UserRoutingComponents=[
+  JobSearchComponent
+];
--- job-portal-frontend/src/app/app.module.ts
+++ job-portal-frontend/src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; 
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { JobSearchComponent } from './job-search/job-search.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';  
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JobSearchComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    CarouselModule,
+    HttpClientModule,
     FormsModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    ToastrModule.forRoot({
+      timeOut: 10000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+      progressBar: true
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
