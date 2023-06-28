23d73449c94bb4b0c62e4ffd4dc62743a77eb092
pavan-33 <321910302033@gitam.in>
2023-06-28 12:08:23 +0000

    commit

---
+++ angularapp/angularapp/src/app/jobseekerdetails/jobseekerdetailsservice.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JobseekerdetailsserviceService {
+  private baseUrl = 'https://8080-becfabfadacaeaebfceaeaadbdbabf.project.examly.io/'; 
+
+  constructor(private httpCli: HttpClient) { }
+
+  getProfile(jobSeekerId: number): Observable<any> {
+    const url = `${this.baseUrl}/details/job-seekers/${jobSeekerId}`;
+    return this.httpCli.get(url);
+  }
+}
