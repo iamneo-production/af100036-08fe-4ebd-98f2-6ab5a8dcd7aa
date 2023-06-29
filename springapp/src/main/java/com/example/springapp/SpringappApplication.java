a73c244c02eff5c5ffd15b12ba3f151f8c0a7c04
sadguruvamsi <sadguruvamsi@gmail.com>
2023-06-29 11:13:15 +0000

    commit

--- springapp/src/main/java/com/example/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/example/springapp/SpringappApplication.java
@@ -3,18 +3,6 @@ package com.example.springapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
-import org.springframework.context.annotation.Configuration;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-
-
-
-@Configuration
-@ComponentScan(basePackages = {"com.example.springapp.controller"})
-@EnableAutoConfiguration
 @SpringBootApplication
 public class SpringappApplication {
 
