65385b246f91bdc374dc1533d4b63603848e1b4b
dhivyas24 <dhivyas2424@gmail.com>
2023-07-01 08:07:32 +0000

    commit

--- springapp/src/main/java/com/example/springapp/model/Employers.java
+++ springapp/src/main/java/com/example/springapp/model/Employers.java
@@ -5,8 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import javax.persistence.Column;
-import org.hibernate.annotations.Cascade;
+
 import org.hibernate.annotations.Filter;
 import org.hibernate.annotations.FilterDef;
 import org.hibernate.annotations.ParamDef;
@@ -14,23 +13,23 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 import java.time.LocalDateTime;
 
 import java.util.List;
-
-
 @Entity
-@Table(name = "job_seeker")
+@Table(name = "employers")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @FilterDef(name = "deletedFilter", parameters = @ParamDef(name = "isDeleted", type = "boolean"))
 @Filter(name = "deletedFilter", condition = "deleted = :isDeleted")
-public class JobSeeker {
+public class Employers {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -39,35 +38,21 @@ public class JobSeeker {
     @Column(name = "name")
     private String name;
 
-    @ElementCollection
-    @CollectionTable(name = "job_seeker_skills", joinColumns = @JoinColumn(name = "job_seeker_id"))
-    @Column(name = "skill")
-    private List<String> skills;
-
-    @Column(name = "experience")
-    private Integer experience;
+    @Column(name = "description", columnDefinition = "LONGTEXT")
+    private String description;
 
     @Column(name = "location")
     private String location;
 
-    @Column(name = "marital_status")
-    private String maritalStatus;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "gender")
+    private String gender;
 
-    @Column(name = "hometown")
-    private String hometown;
-
-    @Column(name = "nationality")
-    private String nationality;
-
-    @Column(name = "last_job_designation")
-    private String lastJobDesignation;
-
-    @Column(name = "reasons_for_leaving")
-    private String reasonsForLeaving;
-
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
 
     @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
@@ -80,16 +65,7 @@ public class JobSeeker {
     @Column(name = "deleted", nullable = false, updatable = true)
     private Boolean deleted = false;
 
-    @Column(name = "reported", nullable = false, updatable = true)
-    private Boolean reported = false;
-
-    @JsonIgnoreProperties("jobSeeker")
-    @OneToMany(mappedBy = "jobSeeker", cascade = CascadeType.ALL)
-    private List<JobApplication> applications;
-
-       @Column(name = "mobile")
-    private String mobile;
-        @Column(name = "lastjob")
-    private String lastjob;
-
+    @OneToMany(mappedBy = "employer", cascade ={ CascadeType.REMOVE, CascadeType.ALL,CascadeType.PERSIST})
+    @JsonIgnoreProperties("employer")
+    private List<Job> jobs;
 }
---
+++ springapp/src/main/java/com/example/springapp/service/UserService.java
@@ -0,0 +1,29 @@
+package com.example.springapp.service;
+
+import com.example.springapp.model.User;
+import com.example.springapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User createUser(User user) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        return userRepository.save(user);
+    }
+}
