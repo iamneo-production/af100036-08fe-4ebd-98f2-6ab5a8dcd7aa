0cf5135f78591f36d5b04b41782148c09e478af3
dhivyas24 <dhivyas2424@gmail.com>
2023-07-05 14:32:30 +0000

    commit

--- angularapp/angularapp/src/app/app.module.ts
+++ angularapp/angularapp/src/app/app.module.ts
@@ -1,26 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AddemployerformComponentComponent } from './admin-dashboard/addemployerform-component/addemployerform-component.component';
-
 import { AddjobformComponentComponent } from './admin-dashboard/addjobform-component/addjobform-component.component';
 import { AddjobseekerformComponentComponent } from './admin-dashboard/addjobseekerform-component/addjobseekerform-component.component';
 import { CmsComponentComponent } from './admin-dashboard/cms-component/cms-component.component';
 import { EmployeranalyticsComponentComponent } from './admin-dashboard/employeranalytics-component/employeranalytics-component.component';
-
 import { JobcompoentComponentComponent } from './admin-dashboard/jobcompoent-component/jobcompoent-component.component';
 import { JobseekercompoentComponentComponent } from './admin-dashboard/jobseekercompoent-component/jobseekercompoent-component.component';
 import { EmployercomponentComponent } from './admin-dashboard/employercomponent/employercomponent.component';
 import { FaqformComponentComponent } from './admin-dashboard/faqform-component/faqform-component.component';
 import { SidebarComponentComponent } from './admin-dashboard/sidebar-component/sidebar-component.component';
 import { TaskComponentComponent } from './admin-dashboard/task-component/task-component.component';
-
 import { HttpClientModule } from '@angular/common/http';
-
 import { NgChartsModule } from 'ng2-charts';
 import { JobApplicationComponent } from './job-application/job-application.component';
 import { JobseekerprofileComponent } from './jobseekerdetails/jobseekerprofile/jobseekerprofile.component';
@@ -32,12 +27,9 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
-<<<<<<< HEAD
-import { EmployerPageComponent } from './employer-dashboard/employer-dashboard.component';
-=======
+import { EmployerDashboardComponent } from './employer-dashboard/employer-dashboard.component';
 import { JobDetailsComponent } from './job-details/job-details.component';
 
->>>>>>> ef805b1a2678dd0db5de99f0246fe6b8834f9483
 
 
 @NgModule({
@@ -61,11 +53,8 @@ import { JobDetailsComponent } from './job-details/job-details.component';
     JobseekereditprofileComponent,
     SignupComponent,
     LoginComponent,
-<<<<<<< HEAD
-    EmployerPageComponent
-=======
+    EmployerDashboardComponent,
     JobDetailsComponent
->>>>>>> ef805b1a2678dd0db5de99f0246fe6b8834f9483
   
   ],
   imports: [
